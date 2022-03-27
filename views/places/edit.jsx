6761b2dd850cfa1c21bf1abd4ce0fa48e571a668
root <mtrhoades0727@gmail.com>
2022-03-27 12:46:10 -1000

    added the edit/update functionality

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -4,10 +4,39 @@ const Def = require('../default'); // default html import
 
 
 // Stub Function
-function edit () {
+function edit (data) {
     return (
         <Def>
-            <h1>Edit a place page!</h1>
+            <main>
+                <h1>Edit a place page!</h1>
+                <form method="POST" action={`/places/${data.index}?_method=PUT`}>
+                <div className="row">
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="name">Place Name</label>
+                        <input className="form-control" id="name" name="name" value={data.name} />
+                    </div>
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input className="form-control" id="pic" name="pic" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="city">City</label>
+                        <input className="form-control" id="city" name="city" />
+                    </div>
+                    <div className="form-group col-sm-6">
+                        <label htmlFor="state">State</label>
+                        <input className="form-control" id="state" name="state" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="cuisines">Cuisines</label>
+                    <input className="form-control" id="cuisines" name="cuisines" />
+                </div>
+                <input className="btn btn-primary" type="submit" value="Submit" />
+                </form>
+            </main>
         </Def>
     )
 }
