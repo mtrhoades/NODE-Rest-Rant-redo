c52a42db5ffa18f76d6d3728cc57e09ac47b1bda
root <mtrhoades0727@gmail.com>
2022-03-28 13:44:10 -1000

    added helper method and bakers

--- models/index.js
+++ models/index.js
@@ -5,6 +5,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 });
+console.log('connected to MONGO')
+
 
 // EXPORTS
 module.exports.Place = require('./places')
